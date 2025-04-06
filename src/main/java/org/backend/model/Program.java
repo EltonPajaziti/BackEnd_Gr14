13b5d095554781d91185220d1b1508a92cb34d98
Leutrim Hajdini <leutrimhajdini63@gmail.com>
2025-04-06 15:54:00 +0200

    KAN-4 Krijimi i modelit Program, Repository, Service dhe Controller

---
+++ src/main/java/org/backend/model/Program.java
@@ -0,0 +1,88 @@
+package org.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "program")
+public class Program {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "level", length = 50, nullable = false)
+    private String level;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "department_id", nullable = false)
+    //private Department department;
+
+    @Column(name = "tenant_id")
+    private Integer tenantId;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public Program() {};
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getLevel(){
+        return level;
+    }
+    public void setLevel(String level){
+        this.level = level;
+    }
+
+    /*
+
+    public Department getDepartment() {
+        //return department;
+    }
+
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    Pasi te shtohet modeli Department
+
+    */
+
+    public int getTenantId(){
+        return tenantId;
+    }
+    public void setTenantId(int tenantId){
+        this.tenantId = tenantId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+}
---
+++ src/main/java/org/backend/repository/ProgramRepository.java
@@ -0,0 +1,15 @@
+package org.backend.repository;
+
+import org.backend.model.Program;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProgramRepository extends JpaRepository<Program, Long> {
+
+    Program findProgramByName(String name);
+    Program findProgramByLevel(String level);
+    //List<Program> findByDepartmentId(Long departmentId); Pasi te shtohet modeli Department
+
+
+}
