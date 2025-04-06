156f9a11bf256c268b3ed41995325471dccc3887
ZanaShabani <zana.shabani2@student.uni-pr.edu>
2025-04-06 17:25:46 +0200

    KAN-9 Krijimi i model, repository, service te departamentit

---
+++ src/main/java/org/backend/service/DepartmentService.java
@@ -0,0 +1,52 @@
+package org.backend.service;
+
+import org.backend.model.Department;
+import org.backend.repository.DepartmentRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DepartmentService {
+
+    private  final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository){
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<Department> getAllDepartments(){
+        return  departmentRepository.findAll();
+    }
+
+    public Optional<Department> getDepartmentById(Long id){
+        return  departmentRepository.findById(id);
+    }
+
+    public Department getDepartmentByName(String name){
+        return departmentRepository.findByName(name);
+    }
+
+    public List<Department> getDepartmentsByFacultyId(Long facultyId){
+        return departmentRepository.findByFacultyId(facultyId);
+    }
+
+    public Department createDepartment(Department department){
+        return  departmentRepository.save(department);
+    }
+
+    public Department updateDepartment(Long id, Department updateDepartment){
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Departamenti nuk u gjet"));
+
+        department.setName(updateDepartment.getName());
+        department.setFaculty(updateDepartment.getFaculty());
+
+        return departmentRepository.save(department);
+
+    }
+
+    public  void deleteDepartment(Long id){
+        departmentRepository.deleteById(id);
+    }
+}
